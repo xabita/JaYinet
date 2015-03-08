83f2bf92b627cce301150bd684bd5ee4a254d9e4
xabita <mutskxabaa@gmail.com>
2015-03-08 01:47:27 -0600

    Initial Commit

---
+++ JaYinet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JaYinet
+//
+//  Created by Elizabeth Martínez Gómez on 08/03/15.
+//  Copyright (c) 2015 Elizabeth Martínez Gómez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JaYinet/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JaYinet
+//
+//  Created by Elizabeth Martínez Gómez on 08/03/15.
+//  Copyright (c) 2015 Elizabeth Martínez Gómez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
